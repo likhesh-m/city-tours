eeaddbef8897e270396b88d3164973d4447de912
likhesh28 <likhesh@live.com>
2020-01-30 13:58:29 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Navbar from './components/Navbar/Navbar';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
